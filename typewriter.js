8fce6ba15a48a4266f3e37971b3fbc5007b7028b
dacharya64 <dacharya64@gmail.com>
2016-08-07 17:38:12 -0400

    putting up progress so far
    +background
    +panels
    +modals
    +text transition js

---
+++ typewriter.js
@@ -0,0 +1,18 @@
+var str = "<p>I never thought it could actually happen, but... she's disappeared. Just like all the others. </p>",
+    i = 0,
+    isTag,
+    text;
+
+(function type() {
+    text = str.slice(0, ++i);
+    if (text === str) return;
+
+    document.getElementById('typewriter').innerHTML = text;
+
+    var char = text.slice(-1);
+    if( char === '<' ) isTag = true;
+    if( char === '>' ) isTag = false;
+
+    if (isTag) return type();
+    setTimeout(type, 30);
+}());
